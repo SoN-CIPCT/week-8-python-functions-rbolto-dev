223556fbf7a383fe28bdf90e4e73fed616fc2bf0
rbolto-dev <rbolto@uw.edu>
2026-02-24 22:47:42 +0000

    Week 8 python complete

---
+++ homework8_robert_bolton.py
@@ -0,0 +1,8 @@
+def forge_millennium_falcon_melt(*ingredients):
+    print("Preparing your Millenium Falcon Melt with the following ingredients:")
+    for item in ingredients:
+        print("-",item)
+
+forge_millennium_falcon_melt("bantha bacon","Endor cheddar","Yoda peppers")
+forge_millennium_falcon_melt("wookie turkey","death star pickles")
+
